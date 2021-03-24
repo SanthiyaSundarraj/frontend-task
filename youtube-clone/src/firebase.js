1c75221b08ab77658257d1b66679377e9632c051
Santhiya Sundarraj <56165286+SanthiyaSundarraj@users.noreply.github.com>
2021-03-24 16:09:35 +0530

    Update firebase.js

--- youtube-clone/src/firebase.js
+++ youtube-clone/src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app'
 
 import 'firebase/auth'
 
-const firebaseConfig = {
-   apiKey: 'AIzaSyCOoquLockIxYZVE5nreKPDMDbVTh6KT-U',
-   authDomain: 'not-yt.firebaseapp.com',
-   projectId: 'not-yt',
-   storageBucket: 'not-yt.appspot.com',
-   messagingSenderId: '713650135776',
-   appId: '1:713650135776:web:3612f40b495bd9e6ef6685',
+const firebaseConfig = {apiKey: "AIzaSyDkfotQ93ynzKlSceiX5_ioUcdZ2JD8zII",
+  authDomain: "fir-3ac3a.firebaseapp.com",
+  projectId: "fir-3ac3a",
+  storageBucket: "fir-3ac3a.appspot.com",
+  messagingSenderId: "487549600614",
+  appId: "1:487549600614:web:c31d90dc6d89add1f8bea7",
+  measurementId: "G-SLXNXHJVNE",
 }
 
 firebase.initializeApp(firebaseConfig)
